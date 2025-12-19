1a86349843d741d773dd2e166f22779d1186cfd7
bvanseg <bvanseg@gmail.com>
2025-12-19 07:45:05 -0500

    ♻️ Refactor.
    Signed-off-by: = <bvanseg@gmail.com>

--- neoforge/src/main/java/com/human/neoforge/client/HumanNeoForgeClient.java
+++ neoforge/src/main/java/com/human/neoforge/client/HumanNeoForgeClient.java
@@ -3,15 +3,12 @@ package com.human.neoforge.client;
 import com.human.Human;
 import com.human.client.HumanClient;
 import net.neoforged.api.distmarker.Dist;
-import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 
 @Mod(value = Human.MOD_ID, dist = Dist.CLIENT)
 public class HumanNeoForgeClient {
 
-    static {}
-
-    public HumanNeoForgeClient(IEventBus modBus) {
+    public HumanNeoForgeClient() {
         HumanClient.initialize();
     }
 }
