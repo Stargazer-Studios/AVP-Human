1ec677b2041c2124a70839d46d88e9ac20172601
bvanseg <bvanseg@gmail.com>
2025-12-22 20:48:08 -0500

    🛠 Added predator module for compatibility.
    Signed-off-by: = <bvanseg@gmail.com>

---
+++ common/src/main/java/com/human/compatibility/avp_predator/AVPPredator.java
@@ -0,0 +1,10 @@
+package com.human.compatibility.avp_predator;
+
+import com.blib.BLib;
+import com.blib.BLibMod;
+
+public class AVPPredator {
+
+    public static final BLibMod MOD = BLib.createMod("avp_predator");
+
+}
