5653a0f2b1130b144ae6791b0b7f9f92294105ab
bvanseg <bvanseg@gmail.com>
2025-12-29 20:39:16 -0500

    🐞 Fixed water placement spam when marines try to extinguish themselves.
    Signed-off-by: = <bvanseg@gmail.com>

--- common/src/main/java/com/human/common/gameplay/entity/living/human/marine/ai/extinguish_fire/action/PlaceWaterAtFeetAction.java
+++ common/src/main/java/com/human/common/gameplay/entity/living/human/marine/ai/extinguish_fire/action/PlaceWaterAtFeetAction.java
@@ -8,50 +8,80 @@ import com.just.goap.StateKey;
 import com.just.goap.state.Blackboard;
 import com.just.goap.state.ReadableWorldState;
 import net.minecraft.core.BlockPos;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import org.jetbrains.annotations.NotNull;
 
 public class PlaceWaterAtFeetAction {
 
-    private static final Item WATER_BUCKET_ITEM = Items.WATER_BUCKET;
-
     private static final InteractionHand HAND_TO_USE = InteractionHand.MAIN_HAND;
+
+    private static final int INTERACT_RANGE_IN_BLOCKS = 3;
 
     private static final StateKey.Derived<Option<BlockPos>> WATER_POS_OPTION = StateKey.derived("water_pos_option");
 
     public static Action.Signal perform(Marine marine, ReadableWorldState worldState, Blackboard blackboard) {
         var mainhandItemStack = marine.getMainHandItem();
-        var isWaterBucketEquipped = mainhandItemStack.is(WATER_BUCKET_ITEM);
+        var isWaterBucketEquipped = mainhandItemStack.is(Items.WATER_BUCKET);
 
         if (!isWaterBucketEquipped) {
             return equipWaterBucket(marine, mainhandItemStack);
         }
 
-        var blockPos = marine.blockPosition();
-        blackboard.set(WATER_POS_OPTION, Option.some(blockPos));
-        marine.level().setBlock(blockPos, Blocks.WATER.defaultBlockState(), Block.UPDATE_ALL);
-        marine.setItemInHand(HAND_TO_USE, new ItemStack(Items.BUCKET));
+        var currentWaterPosOption = blackboard.getOrDefault(WATER_POS_OPTION, Option.none());
+
+        if (currentWaterPosOption.isNone()) {
+            var blockPos = getBestPosForWaterPlacement(marine);
+            currentWaterPosOption = Option.some(blockPos);
+
+            blackboard.set(WATER_POS_OPTION, currentWaterPosOption);
+
+            marine.level().setBlock(blockPos, Blocks.WATER.defaultBlockState(), Block.UPDATE_ALL);
+            marine.setItemInHand(HAND_TO_USE, new ItemStack(Items.BUCKET));
+        }
 
         return Action.Signal.CONTINUE;
+    }
+
+    private static @NotNull BlockPos getBestPosForWaterPlacement(Marine marine) {
+        return BlockPos.betweenClosedStream(marine.getBoundingBox())
+            .filter(blockPos -> marine.level().getBlockState(blockPos).is(BlockTags.FIRE))
+            .findFirst()
+            .orElse(marine.blockPosition());
     }
 
     public static void onFinish(Marine marine, ReadableWorldState worldState, Blackboard blackboard) {
         var waterPosOption = blackboard.getOrDefault(WATER_POS_OPTION, Option.none());
 
         waterPosOption.ifSome(blockPos -> {
-            marine.level().setBlock(marine.blockPosition(), Blocks.AIR.defaultBlockState(), Block.UPDATE_ALL);
+            var blockState = marine.level().getBlockState(blockPos);
+
+            if (
+                blockState.is(Blocks.WATER)
+                    && marine.distanceToSqr(blockPos.getCenter()) <= INTERACT_RANGE_IN_BLOCKS * INTERACT_RANGE_IN_BLOCKS
+            ) {
+                // Block state is water AND marine is close enough to pick up block state.
+                // Set the block state to air.
+                marine.level().setBlock(blockPos, Blocks.AIR.defaultBlockState(), Block.UPDATE_ALL);
+                // Add the water bucket back to the marine's inventory.
+                marine.getInventory().addItem(Items.WATER_BUCKET);
+            } else {
+                // Otherwise block state is not water OR marine is too far away from it to pick it up, so return an
+                // empty bucket to the marine's inventory.
+                marine.getInventory().addItem(Items.BUCKET);
+            }
+
             marine.setItemInHand(HAND_TO_USE, ItemStack.EMPTY);
-            marine.getInventory().addItem(WATER_BUCKET_ITEM);
         });
     }
 
     private static Action.Signal equipWaterBucket(Marine marine, ItemStack mainhandItemStack) {
         // Remove the water bucket from the marine's inventory.
-        var removeResult = marine.getInventory().removeItem(WATER_BUCKET_ITEM);
+        var removeResult = marine.getInventory().removeItem(Items.WATER_BUCKET);
 
         return switch (removeResult) {
             case BLibInventory.RemoveResult.InventoryEmpty inventoryEmpty -> Action.Signal.ABORT;
@@ -61,7 +91,7 @@ public class PlaceWaterAtFeetAction {
                 marine.getInventory().addItemStack(mainhandItemStack);
                 marine.setItemInHand(HAND_TO_USE, ItemStack.EMPTY);
                 // Equip water bucket.
-                marine.setItemInHand(HAND_TO_USE, new ItemStack(WATER_BUCKET_ITEM));
+                marine.setItemInHand(HAND_TO_USE, new ItemStack(Items.WATER_BUCKET));
 
                 yield Action.Signal.CONTINUE;
             }
