ca3b4be11ef9e29553939b0684f22e2ebad2bf7a
bvanseg <bvanseg@gmail.com>
2026-01-12 22:58:51 -0500

    🚧 Committing progress.
    Signed-off-by: = <bvanseg@gmail.com>

--- common/src/main/java/com/human/client/render/item/gun/muzzled/impl/M42A3SniperRifleItemRenderer.java
+++ common/src/main/java/com/human/client/render/item/gun/muzzled/impl/M42A3SniperRifleItemRenderer.java
@@ -2,6 +2,7 @@ package com.human.client.render.item.gun.muzzled.impl;
 
 import com.human.client.animation.item.M42A3SniperRifleAnimator;
 import com.human.client.render.item.gun.muzzled.MuzzledGunItemRenderer;
+import net.minecraft.world.item.ItemDisplayContext;
 
 public class M42A3SniperRifleItemRenderer extends MuzzledGunItemRenderer {
 
@@ -9,6 +10,9 @@ public class M42A3SniperRifleItemRenderer extends MuzzledGunItemRenderer {
         super(
             name,
             config -> config
+                .disableAnimationInContexts(ItemDisplayContext.GUI)
+                // Custom animation logic with predicate
+                .setShouldAnimateInContext(context -> context != ItemDisplayContext.GUI && context != ItemDisplayContext.FIXED)
                 .setAnimatorProvider(M42A3SniperRifleAnimator::new)
         );
     }
