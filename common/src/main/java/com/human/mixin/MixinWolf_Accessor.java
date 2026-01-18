9890e277d67d355aea895461e912e4ce08a3c753
bvanseg <bvanseg@gmail.com>
2026-01-18 08:56:22 -0500

    ✨ Added MixinWolf_Accessor.
    Signed-off-by: = <bvanseg@gmail.com>

---
+++ common/src/main/java/com/human/mixin/MixinWolf_Accessor.java
@@ -0,0 +1,14 @@
+package com.human.mixin;
+
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.world.entity.animal.Wolf;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(Wolf.class)
+public interface MixinWolf_Accessor {
+
+    @Accessor(value = "DATA_COLLAR_COLOR")
+    EntityDataAccessor<Integer> getDataCollarColor();
+
+}
