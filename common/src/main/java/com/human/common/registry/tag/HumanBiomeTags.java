9e15a12c96ae0ef87a05aa734bfd3aec2cf80b07
bvanseg <bvanseg@gmail.com>
2026-01-22 14:35:50 -0500

    ✨ Added new biome tags for marine patrol spawn locations.
    Signed-off-by: = <bvanseg@gmail.com>

--- common/src/main/java/com/human/common/registry/tag/HumanBiomeTags.java
+++ common/src/main/java/com/human/common/registry/tag/HumanBiomeTags.java
@@ -9,6 +9,8 @@ public class HumanBiomeTags {
 
     public static final TagKey<Biome> HAS_MARINE_CAMP_GRASS = create("has_marine_camp_grass");
 
+    public static final TagKey<Biome> HAS_MARINE_PATROLS = create("has_marine_patrols");
+
     public static final TagKey<Biome> HAS_MOBILE_LAB = create("has_mobile_lab");
 
     public static final TagKey<Biome> HAS_OUTPOST_COMMS = create("has_outpost_comms");
@@ -18,6 +20,18 @@ public class HumanBiomeTags {
     public static final TagKey<Biome> HAS_OUTPOST_SUPPLY_BADLAND = create("has_outpost_supply_badland");
 
     public static final TagKey<Biome> HAS_OUTPOST_SUPPLY_DESERT = create("has_outpost_supply_desert");
+
+    public static final TagKey<Biome> HAS_TACTICAL_MARINE_PATROLS = create("has_tactical_marine_patrols");
+
+    public static final TagKey<Biome> HAS_WY_APE_PATROLS = create("has_wy_ape_patrols");
+
+    public static final TagKey<Biome> HAS_WY_COMMANDO_PATROLS = create("has_wy_commando_patrols");
+
+    public static final TagKey<Biome> HAS_WY_ELITE_PATROLS = create("has_wy_elite_patrols");
+
+    public static final TagKey<Biome> HAS_WY_SPEC_OPS_COMMANDO_PATROLS = create("has_wy_spec_ops_commando_patrols");
+
+    public static final TagKey<Biome> HAS_WY_SPEC_OPS_ELITE_PATROLS = create("has_wy_spec_ops_elite_patrols");
 
     public static final TagKey<Biome> IS_IRRADIATED = create("is_irradiated");
 
